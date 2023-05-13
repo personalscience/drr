97a6298becc969a2785586c854733fcb6bb2cb5b
Richard Sprague <richardsprague@gmail.com>
2023-05-13 13:29:48 -0700

    uses formik for forms

--- frontend/drr_frontend/src/Input.js
+++ frontend/drr_frontend/src/Input.js
@@ -1,127 +1,137 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import axios from 'axios';
-
-import logo from './assets/psi-testtube.svg'; // Replace 'logo.png' with your actual logo file name and extension
-
-// import `useNavigate` hook from `react-router-dom`
 import { useNavigate } from 'react-router-dom';
 
+const validationSchema = yup.object({
+  age: yup.number().required("Age is required"),
+  sex: yup.string().required("Sex is required"),
+  height: yup.number().required("Height is required"),
+  weight: yup.number().required("Weight is required"),
+  bloodData: yup.string().required("Blood Data is required"),
+  familyHistoryData: yup.string().required("Family History Data is required"),
+});
+
 const Input = () => {
-  const [age, setAge] = useState('');
-  const [sex, setSex] = useState('');
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
-  const [message, setMessage] = useState('');
-  const [bmi, setBmi] = useState('');
-  const [bloodData, setBloodData] = useState('');
-  const [familyHistoryData, setFamilyHistoryData] = useState('');
+  const navigate = useNavigate();
 
-  const navigate = useNavigate(); // Initialize `navigate`
+  const formik = useFormik({
+    initialValues: {
+      age: '',
+      sex: '',
+      height: '',
+      weight: '',
+      bloodData: '',
+      familyHistoryData: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5001';
 
+        const response = await axios.post(`${backendUrl}/api/calculate_bmi`, {
+          height: Number(values.height),
+          weight: Number(values.weight)
+        });
 
-  useEffect(() => {
-    if (bmi) {
-      setMessage(`Received user information: Age: ${age}, Sex: ${sex}, Height: ${height}, Weight: ${weight}, BMI: ${bmi}`);
-    }
-  }, [bmi]);  // Run the effect whenever `bmi` changes
+        const receivedBmi = response.data.bmi;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+        const recommendationResponse = await axios.post(`${backendUrl}/recommendation`, values);
 
-    try {
-      const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5001';
+        const recommendation = recommendationResponse.data.recommendation;
+        const message = `Received user information: Age: ${values.age}, Sex: ${values.sex}, Height: ${values.height}, Weight: ${values.weight}, BMI: ${receivedBmi}`;
 
-      const response = await axios.post(`${backendUrl}/api/calculate_bmi`, {
-        height: Number(height),
-        weight: Number(weight)
-      });
-      console.log(process.env.REACT_APP_BACKEND_URL)
-
-      const receivedBmi = response.data.bmi;
-      setBmi(receivedBmi);
-
-      // Send data to '/recommendation' endpoint
-      const recommendationResponse = await axios.post(`${backendUrl}/recommendation`, {
-        bloodData: bloodData,
-        familyHistoryData: familyHistoryData,
-        age: age,
-        sex: sex,
-        weight: weight,
-        height: height
-
-      });
-
-      const recommendation = recommendationResponse.data.recommendation;  // Extract the received recommendation
-      const message = `Received user information: Age: ${age}, Sex: ${sex}, Height: ${height}, Weight: ${weight}, BMI: ${receivedBmi}`;
-
-      // Pass the message to the results page
-    navigate('/results', { state: { message: message, recommendation: recommendation } });
-    } catch (error) {
-      console.error(error);
-    }
-
-  };
-
-
-
+        navigate('/results', { state: { message: message, recommendation: recommendation } });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setSubmitting(false);
+      }
+    },
+  });
 
   return (
     <div className="container">
       <h1>Health Recommendations</h1>
-
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
+        {/* Rest of your form fields go here, using formik.getFieldProps to bind them */}
+        {/* Here's an example for the "age" field: */}
         <div className="row">
           <div className="col-md-6">
             <div className="form-group">
               <label htmlFor="age">Age:</label>
-              <input type="number" className="form-control" id="age" value={age} onChange={(e) => setAge(e.target.value)} />
+              <input type="number" className="form-control" id="age" {...formik.getFieldProps('age')} />
+              {formik.touched.age && formik.errors.age ? <div>{formik.errors.age}</div> : null}
             </div>
           </div>
+        </div>
+        <div className="row">
+        <div className="col-md-6">
+          <div className="form-group">
+            <label htmlFor="sex">Sex:</label>
+            <select
+              className="form-control"
+              id="sex"
+              {...formik.getFieldProps('sex')}
+            >
+              <option value="">Select...</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+            {formik.touched.sex && formik.errors.sex ? <div>{formik.errors.sex}</div> : null}
+          </div>
+        </div>
+      </div>
+        <div className="row">
           <div className="col-md-6">
             <div className="form-group">
-              <label htmlFor="sex">Sex:</label>
-              <select className="form-control" id="sex" value={sex} onChange={(e) => setSex(e.target.value)}>
-                <option value="">Select...</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </select>
+              <label htmlFor="height">Height:</label>
+              <input type="number" className="form-control" id="height" {...formik.getFieldProps('height')} />
+              {formik.touched.height && formik.errors.height ? <div>{formik.errors.sex}</div> : null}
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col-md-6">
             <div className="form-group">
-              <label htmlFor="height">Height (in cm):</label>
-              <input type="number" className="form-control short-input" id="height" value={height} onChange={(e) => setHeight(e.target.value)} />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="form-group">
-              <label htmlFor="weight">Weight (in kg):</label>
-              <input type="number" className="form-control short-input" id="weight" value={weight} onChange={(e) => setWeight(e.target.value)} />
+              <label htmlFor="weight">Weight:</label>
+              <input type="number" className="form-control" id="weight" {...formik.getFieldProps('weight')} />
+              {formik.touched.weight && formik.errors.weight ? <div>{formik.errors.weight}</div> : null}
             </div>
           </div>
         </div>
         <div className="row">
-          <div className="col-12">
-            <div className="form-group">
-              <h2 className="display-4" htmlFor="bloodData">Enter Blood Test Results</h2>
-              <textarea className="form-control" id="bloodData" rows="10" value={bloodData} onChange={(e) => setBloodData(e.target.value)} />
-            </div>
+        <div className="col-12">
+          <div className="form-group">
+            <label htmlFor="bloodData">Enter Blood Test Results</label>
+            <textarea 
+              className="form-control" 
+              id="bloodData" 
+              rows="10" 
+              {...formik.getFieldProps('bloodData')} 
+            />
+            {formik.touched.bloodData && formik.errors.bloodData ? <div>{formik.errors.bloodData}</div> : null}
           </div>
         </div>
-        <div className="row">
-          <div className="col-12">
-            <div className="form-group">
-              <h2 className="display-4" htmlFor="familyHistoryData">Enter Family History</h2>
-              <textarea className="form-control" id="familyHistoryData" rows="10" value={familyHistoryData} onChange={(e) => setFamilyHistoryData(e.target.value)} />
-            </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div className="form-group">
+            <label htmlFor="familyHistoryData">Enter Family History</label>
+            <textarea 
+              className="form-control" 
+              id="familyHistoryData" 
+              rows="10" 
+              {...formik.getFieldProps('familyHistoryData')} 
+            />
+            {formik.touched.familyHistoryData && formik.errors.familyHistoryData ? <div>{formik.errors.familyHistoryData}</div> : null}
           </div>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+      </div>
+        {/* You would repeat a similar structure for the rest of your fields */}
+        <button type="submit" className="btn btn-primary" disabled={formik.isSubmitting}>Submit</button>
       </form>
-
-      {message && <p>{message}</p>}
     </div>
   );
 };
--- frontend/tests/e2e/test_user_flows.py
+++ frontend/tests/e2e/test_user_flows.py
@@ -3,7 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 import pytest
@@ -15,6 +15,13 @@ def test_submit_form():
     # navigate to your app
     driver.get("http://localhost:3000/input")
 
+    age_field = driver.find_element(By.ID, "age")
+    age_field.clear()
+    age_field.send_keys("25")
+
+    sex_select = Select(driver.find_element(By.ID, "sex"))
+    sex_select.select_by_visible_text("Male")
+
     # find the input field and enter some text
     input_field = driver.find_element(by="id", value="weight")  # replace with the actual name of your input field
     input_field.clear()
@@ -25,24 +32,24 @@ def test_submit_form():
     input_field.clear()
     input_field.send_keys("11")
 
+    bloodData_field = driver.find_element(By.ID, "bloodData")
+    bloodData_field.clear()
+    bloodData_field.send_keys("Some blood data")
+
+    familyHistoryData_field = driver.find_element(By.ID, "familyHistoryData")
+    familyHistoryData_field.clear()
+    familyHistoryData_field.send_keys("Some family history data")
 
     submit_button = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ".btn.btn-primary"))
     )
 
-    # Scroll to the button:
     driver.execute_script("arguments[0].scrollIntoView(true);", submit_button)
     driver.execute_script("arguments[0].click();", submit_button)
 
-
-
-
-    # check that the new page has the expected content
-    # check that the new page has the expected header
     assert "Results" in driver.page_source
+    assert "Received user information: Age: 25, Sex: male, Height: 11, Weight: 55, BMI: 4545.5" in driver.page_source
 
-    # check that the new page displays the user's information
-    assert "Received user information: Age: , Sex: , Height: 11, Weight: 55, BMI: 4545.5" in driver.page_source
 
 
     driver.quit()
